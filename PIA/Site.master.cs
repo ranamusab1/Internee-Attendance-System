d503fb036ec0defbce21544087af6c0eda4e7d85
Rana Muhammad Musab Akram <150699895+ranamusab1@users.noreply.github.com>
2025-07-21 10:30:04 +0500

    Update Site.master.cs

--- PIA/Site.master.cs
+++ PIA/Site.master.cs
@@ -1,24 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+<%@ Page Title="View Interns" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeBehind="ViewInterns.aspx.cs" Inherits="PIA.ViewInterns" %>
 
-namespace PIA
-{
-    public partial class Site : System.Web.UI.MasterPage
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-                if (Session["Username"] != null)
-                    lblUsername.Text = "User: " + Session["Username"].ToString();
-
-                if (Session["Department"] != null)
-                    lblDepartment.Text = "Department: " + Session["Department"].ToString();
-            }
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .table-container {
+            padding: 30px;
+            background-color: rgb(255 255 255 / 0.52);
+            border-radius: 15px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
         }
-    }
-}
+
+        .page-title {
+            text-align: center;
+            align-items: center;
+            font-size: 28px;
+            font-weight: 600;
+            margin-bottom: 25px;
+            color: #004d40;
+        }
+
+        .gridview th, .gridview td {
+            text-align: center;
+            vertical-align: middle;
+        }
+    </style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container-fluid mt-4">
+        <div class="table-container">
+            <h2 class="page-title">Registered Interns</h2>
+            <asp:GridView ID="gvInterns" runat="server" CssClass="table table-bordered table-striped gridview" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="InternID" HeaderText="Intern ID" />
+                    <asp:BoundField DataField="InternName" HeaderText="InternName" />
+                    <asp:BoundField DataField="CNIC" HeaderText="CNIC" />
+                    <asp:BoundField DataField="ContactNumber" HeaderText="Contact Number" />
+                    <asp:BoundField DataField="Department" HeaderText="Department" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
