c5704f626abee419f43f3a130b3793252da08a3b
Rana Muhammad Musab Akram <23-cse-24@students.duet.edu.pk>
2025-07-21 10:33:52 +0500

    updated DataBase

---
+++ PIA1.sql
@@ -0,0 +1,31 @@
+USE PIA;
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Role VARCHAR(20) NOT NULL, -- 'DepartmentAdmin' or 'Intern'
+    Department VARCHAR(50) NOT NULL
+);
+INSERT INTO Users (Username, Password, Role, Department)
+VALUES
+('admin_hr', 'admin123', 'DepartmentAdmin', 'HR'),
+('intern_john', 'pass123', 'Intern', 'HR'),
+('admin_it', 'admin456', 'DepartmentAdmin', 'IT'),
+('intern_sara', 'pass456', 'Intern', 'IT');
+
+select *from Users;
+-- drop table  Interns ;
+CREATE TABLE Interns (
+    InternID VARCHAR(10) PRIMARY KEY,
+    InternName VARCHAR(100),
+    CNIC VARCHAR(15),
+    ContactNumber VARCHAR(15),
+    Email VARCHAR(100),
+    StartDate DATE,
+    EndDate DATE,
+    UniversityName VARCHAR(100),
+    UniversityRollNumber VARCHAR(50),
+    Department VARCHAR(50)
+);
+select * from Interns;
